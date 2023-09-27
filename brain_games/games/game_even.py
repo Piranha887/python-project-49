9340b5dc28976306a795588877313bbc1ce5e394
Piranha887 <ilyanikitin889@gmail.com>
2023-09-27 09:30:04 +0300

    Realize logic of even in game_even.py

---
+++ brain_games/games/game_even.py
@@ -0,0 +1,16 @@
+from random import randint
+
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def check_parity(num):
+    if num % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def generate_number(min_num_rand, max_num_rand):
+    number = randint(min_num_rand, max_num_rand)
+    correct_answer = check_parity(number)
+    return number, correct_answer
