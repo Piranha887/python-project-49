4487acf5b9a924d72777abe852be9f449a9d99bb
Piranha887 <ilyanikitin889@gmail.com>
2023-09-29 13:29:15 +0300

    realize a logic of game_gcd

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,19 @@
+import random
+
+RULES = 'Find the greatest common divisor of given numbers.'
+
+
+def gcd(number1, number2):
+    """Используем алгоритм Евклида для нахождения наибольшего общего делителя"""
+    while number2 != 0:
+        number1, number2 = number2, number1 % number2
+    return str(number1)
+
+
+def generate_question(min_num, max_num):
+    """Генерация случайных чисел, вопроса и правильного ответа"""
+    number1 = random.randint(min_num, max_num)
+    number2 = random.randint(min_num, max_num)
+    question = f'{number1} {number2}'
+    correct_answer = gcd(number1, number2)
+    return question, correct_answer
