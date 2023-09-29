7ca539124ceb0dc45a7ac5de60b28497354ae5a8
Piranha887 <ilyanikitin889@gmail.com>
2023-09-29 12:44:06 +0300

    change game_even.py for improving maintability codeclimate

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -3,14 +3,13 @@ from random import randint
 RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def check_parity(num):
-    if num % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
+def is_even(num):
+    """Проверяет чётность числа"""
+    return 'yes' if num % 2 == 0 else 'no'
 
 
-def generation(min_num_rand, max_num_rand):
-    number = randint(min_num_rand, max_num_rand)
-    correct_answer = check_parity(number)
+def generate_question(min_num, max_num):
+    """Генерирует случайное число и верный ответ"""
+    number = randint(min_num, max_num)
+    correct_answer = is_even(number)
     return number, correct_answer
