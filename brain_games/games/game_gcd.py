61f54b31348eae3ae6431b071a950324b7e7a2cf
Piranha887 <ilyanikitin889@gmail.com>
2023-10-04 11:56:42 +0300

    Edited name of gcd function

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -3,7 +3,7 @@ import random
 RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def gcd(number1, number2):
+def is_gcd(number1, number2):
     """Используем алгоритм Евклида для нахождения наибольшего общего делителя"""
     while number2 != 0:
         number1, number2 = number2, number1 % number2
@@ -15,5 +15,5 @@ def generate_question(min_num, max_num):
     number1 = random.randint(min_num, max_num)
     number2 = random.randint(min_num, max_num)
     question = f'{number1} {number2}'
-    correct_answer = gcd(number1, number2)
+    correct_answer = is_gcd(number1, number2)
     return question, correct_answer
