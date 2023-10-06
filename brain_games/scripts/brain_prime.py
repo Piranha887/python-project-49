12820ded289f0048a14749ddfc5d1a6613aba999
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:11:20 +0300

    restructure brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import game_prime
+from brain_games.games.game_prime import generate_question_prime
 from brain_games.engine import start_game
+from brain_games.constants import RULE_PRIME
 
 
 def main():
-    start_game(game_prime)
+    start_game(RULE_PRIME, generate_question_prime)
 
 
 if __name__ == '__main__':
