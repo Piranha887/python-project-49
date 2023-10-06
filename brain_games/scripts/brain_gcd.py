d91a0ac34560ba6d2ea0816bc1321c78fdcc9db9
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:11:37 +0300

    restructure brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import game_gcd
+from brain_games.games.game_gcd import generate_question_gcd
 from brain_games.engine import start_game
+from brain_games.constants import RULE_GCD
 
 
 def main():
-    start_game(game_gcd)
+    start_game(RULE_GCD, generate_question_gcd)
 
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
