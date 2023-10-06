df84e94c6fd2bbfbf38cac386ee2a8b46d3a0b89
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:06:08 +0300

    create constants.py

---
+++ brain_games/constants.py
@@ -0,0 +1,20 @@
+# константы для engine.py
+COUNT_ATTEMPTS = 3
+# константы для generate_number.py
+MIN_NUM_RAND = 0
+MAX_NUM_RAND = 100
+# правила игр
+RULE_EVEN = 'Answer "yes" if the number is even, otherwise answer "no".'
+RULE_CALC = 'What is the result of the expression?'
+RULE_GCD = 'Find the greatest common divisor of given numbers.'
+RULE_PROGRESSION = 'What number is missing in the progression?'
+RULE_PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+# константы для game_calc.py
+OPERATOR_PLUS = '+'
+OPERATOR_MINUS = '-'
+OPERATOR_MULTIPLY = '*'
+# константы для game_progression.py
+STEP_MIN = 1
+STEP_MAX = 10
+ELEMENT_COUNT_MIN = 5
+ELEMENT_COUNT_MAX = 10
