00849fc870e6dd36a8831514193c2574aafb1e52
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:10:53 +0300

    restructure brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import game_progression
+from brain_games.constants import RULE_PROGRESSION
+from brain_games.games.game_progression import generate_question_progression
 from brain_games.engine import start_game
 
 
 def main():
-    start_game(game_progression)
+    start_game(RULE_PROGRESSION, generate_question_progression)
 
 
 if __name__ == '__main__':
