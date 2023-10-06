71c81f924031538e6ce8cee91117301ca3bdfe70
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:11:51 +0300

    restructure brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
-from brain_games.games import game_even
+from brain_games.games.game_even import generate_question_even
 from brain_games.engine import start_game
+from brain_games.constants import RULE_EVEN
 
 
 def main():
-    start_game(game_even)
+    start_game(RULE_EVEN, generate_question_even)
 
 
 if __name__ == "__main__":
