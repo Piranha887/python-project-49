8457321fd8c0315ebe27ac3d433856e91ce150f8
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:53:27 +0300

    using util of random

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,19 +1,16 @@
-import random
-
-RULES = 'Find the greatest common divisor of given numbers.'
+from brain_games.utils.generate_number import generate_random_number
 
 
 def is_gcd(number1, number2):
-    """Используем алгоритм Евклида для нахождения наибольшего общего делителя"""
+    """Используем алгоритм Евклида для нахождения наиб. общего делителя"""
     while number2 != 0:
         number1, number2 = number2, number1 % number2
     return str(number1)
 
 
-def generate_question(min_num, max_num):
+def generate_question_gcd():
     """Генерация случайных чисел, вопроса и правильного ответа"""
-    number1 = random.randint(min_num, max_num)
-    number2 = random.randint(min_num, max_num)
+    number1, number2 = generate_random_number()
     question = f'{number1} {number2}'
     correct_answer = is_gcd(number1, number2)
     return question, correct_answer
