aa7f28f58faa73d9287f7ae3e1f98fac8a064ef5
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:51:17 +0300

    using constants and util of random

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,24 +1,24 @@
 import random
 
-# Правила игры
-RULES = 'What is the result of the expression?'
+from brain_games.constants import OPERATOR_PLUS, OPERATOR_MINUS, \
+    OPERATOR_MULTIPLY
+from brain_games.utils.generate_number import generate_random_number
 
 
 def calculate_expression(number1, operator, number2):
     """Вычисление результата выражения."""
-    if operator == '+':
+    if operator == OPERATOR_PLUS:
         return number1 + number2
-    elif operator == '-':
+    elif operator == OPERATOR_MINUS:
         return number1 - number2
-    elif operator == '*':
+    elif operator == OPERATOR_MULTIPLY:
         return number1 * number2
 
 
-def generate_question(min_num, max_num):
+def generate_question_calc():
     """Генерация случайного выражения и правильного ответа."""
-    operators = ['+', '-', '*']
-    number1 = random.randint(min_num, max_num)
-    number2 = random.randint(min_num, max_num)
+    operators = [OPERATOR_PLUS, OPERATOR_MINUS, OPERATOR_MULTIPLY]
+    number1, number2 = generate_random_number()
     operator = random.choice(operators)
     expression = f'{number1} {operator} {number2}'
     correct_answer = str(calculate_expression(number1, operator, number2))
