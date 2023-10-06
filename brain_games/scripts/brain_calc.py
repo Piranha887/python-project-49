a1f25ead6cca2ad3fcf7ab957f37727df0643436
Piranha887 <ilyanikitin889@gmail.com>
2023-10-06 19:12:04 +0300

    restructure brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from brain_games.games import game_calc
+from brain_games.games.game_calc import generate_question_calc
 from brain_games.engine import start_game
+from brain_games.constants import RULE_CALC
 
 
 def main():
-    start_game(game_calc)
+    start_game(RULE_CALC, generate_question_calc)
 
 
 if __name__ == '__main__':
