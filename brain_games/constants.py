3213b6ffb1d51e155ab3b8472bef60767aa90dc5
Piranha887 <ilyanikitin889@gmail.com>
2023-10-11 13:04:23 +0300

    refactor constants.py

--- brain_games/constants.py
+++ brain_games/constants.py
@@ -9,12 +9,5 @@ RULE_CALC = 'What is the result of the expression?'
 RULE_GCD = 'Find the greatest common divisor of given numbers.'
 RULE_PROGRESSION = 'What number is missing in the progression?'
 RULE_PRIME = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-# константы для game_calc.py
-OPERATOR_PLUS = '+'
-OPERATOR_MINUS = '-'
-OPERATOR_MULTIPLY = '*'
 # константы для game_progression.py
-STEP_MIN = 1
-STEP_MAX = 10
-ELEMENT_COUNT_MIN = 5
-ELEMENT_COUNT_MAX = 10
+PROGRESSION_LENGTH = 10
