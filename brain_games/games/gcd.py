7a1f397b7ee8630e3c80b057555d130c86937229
Piranha887 <ilyanikitin889@gmail.com>
2023-10-11 12:57:12 +0300

    refactor gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,17 +1,15 @@
-from brain_games.utils.generate_number import generate_random_number
+import math
+from brain_games.utils import generate_random_number
 
 
-def is_gcd(number1, number2):
-    """Используем алгоритм Евклида для нахождения наиб. общего делителя"""
-    while number2 != 0:
-        number1, number2 = number2, number1 % number2
-    return str(number1)
+def find_gcd(number1, number2) -> str:
+    """Находит наибольший общий делитель двух чисел"""
+    return str(math.gcd(number1, number2))
 
 
 def generate_question_gcd():
     """Генерация случайных чисел, вопроса и правильного ответа"""
-    number1 = generate_random_number()
-    number2 = generate_random_number()
+    number1, number2 = generate_random_number()
     question = f'{number1} {number2}'
-    correct_answer = is_gcd(number1, number2)
+    correct_answer = find_gcd(number1, number2)
     return question, correct_answer
