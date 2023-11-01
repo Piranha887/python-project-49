bbec34313d0bc68b3d796bd694a3d89b89c82d38
Piranha887 <ilyanikitin889@gmail.com>
2023-11-01 09:42:15 +0300

    Move the startup function down in the code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,10 +3,6 @@ from brain_games.engine import start_game
 from brain_games.utils import generate_random_number
 
 
-def start_prime_game():
-    return start_game(RULE_PRIME, generate_question_prime)
-
-
 def is_prime(number) -> bool:
     """Check if a number is prime"""
     if number < 2:
@@ -21,3 +17,7 @@ def generate_question_prime():
     number = generate_random_number()
     correct_answer = "yes" if is_prime(number) else "no"
     return number, correct_answer
+
+
+def start_prime_game():
+    return start_game(RULE_PRIME, generate_question_prime)
