00ee22c5264378e03d7d9ee2e2337e2e6563486f
Piranha887 <ilyanikitin889@gmail.com>
2023-11-01 09:43:21 +0300

    Working with abstraction

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,18 +5,24 @@ from brain_games.engine import start_game
 from brain_games.utils import generate_random_number
 
 
-def start_gcd_game():
-    return start_game(RULE_GCD, generate_question_gcd)
+def calculate_gcd(number1, number2) -> int:
+    """Calculate the greatest common divisor of two numbers."""
+    return math.gcd(number1, number2)
 
 
-def find_gcd(number1, number2) -> str:
-    """Finds the greatest common divisor of two numbers"""
-    return str(math.gcd(number1, number2))
+def get_gcd_as_string(number1, number2) -> str:
+    """Get the greatest common divisor of two numbers as a string."""
+    gcd_value = calculate_gcd(number1, number2)
+    return str(gcd_value)
 
 
 def generate_question_gcd():
     """Generates random numbers, a question, and the correct answer"""
     number1, number2 = generate_random_number()
     question = f'{number1} {number2}'
-    correct_answer = find_gcd(number1, number2)
+    correct_answer = get_gcd_as_string(number1, number2)
     return question, correct_answer
+
+
+def start_gcd_game():
+    return start_game(RULE_GCD, generate_question_gcd)
