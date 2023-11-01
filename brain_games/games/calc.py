0b1ede0916ba82e639980f6027f741304f2f7111
Piranha887 <ilyanikitin889@gmail.com>
2023-11-01 09:45:09 +0300

    Reconstruct calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,33 +1,33 @@
 import random
 
-from brain_games.constants import RULE_CALC
+from brain_games.constants import RULE_CALC, OPERATORS
 from brain_games.engine import start_game
 from brain_games.utils import generate_random_number
 
 
-def start_calc_game():
-    return start_game(RULE_CALC, generate_question_calc)
-
-
-def calculate_expression_and_generate_operator(number1, operators, number2) -> list[str]:
+def calculate_expression(number1, operator, number2):
     """Calculate the result of an expression."""
-    operator = random.choice(operators)
-    if operator == '+':  # Using string symbols instead of constants
-        return ['+', str(number1 + number2)]
+    if operator == '+':
+        result = number1 + number2
     elif operator == '-':
-        return ['-', str(number1 - number2)]
+        result = number1 - number2
     elif operator == '*':
-        return ['*', str(number1 * number2)]
+        result = number1 * number2
     else:
         raise ValueError("Unsupported operator")
+
+    return result
 
 
 def generate_question_calc():
     """Generate a random expression and the correct answer."""
     number1, number2 = generate_random_number()
-    operators = ['+', '-', '*']
-    correct_answer = calculate_expression_and_generate_operator(number1, operators, number2)
-    operator = correct_answer[0]
+    operator = random.choice(OPERATORS)
+    correct_answer = calculate_expression(number1, operator, number2)
     expression = f'{number1} {operator} {number2}'
 
-    return expression, correct_answer[1]
+    return expression, correct_answer
+
+
+def start_calc_game():
+    return start_game(RULE_CALC, generate_question_calc)
