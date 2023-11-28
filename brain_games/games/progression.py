f898c2ab0389d906c14974e7fdab10bbadfb1727
Piranha887 <ilyanikitin889@gmail.com>
2023-11-28 10:54:24 +0300

    flake8 refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,9 +5,11 @@ from brain_games.engine import start_game
 from brain_games.utils import generate_random_number
 
 
-def generate_arithmetic_progression(first_num, diff, missed_num_ind, progression_length):
+def generate_arithmetic_progression(first_num, diff, missed_num_ind,
+                                    progression_length):
     """Generate an arithmetic progression with a missing element"""
-    progression = ['..' if i == missed_num_ind else str(first_num + i * diff)
+    progression = ['..' if i == missed_num_ind else
+                   str(first_num + i * diff)
                    for i in range(progression_length)]
     return ' '.join(progression)
 
@@ -19,7 +21,8 @@ def get_missing_number_and_progression_result():
     missed_num_ind = randint(0, PROGRESSION_LENGTH - 1)
 
     progression = generate_arithmetic_progression(first_num,
-                                                  diff, missed_num_ind, PROGRESSION_LENGTH)
+                                                  diff, missed_num_ind,
+                                                  PROGRESSION_LENGTH)
 
     question = progression
     correct_answer = str(first_num + missed_num_ind * diff)
@@ -28,4 +31,5 @@ def get_missing_number_and_progression_result():
 
 
 def start_progression_game():
-    return start_game(RULE_PROGRESSION, get_missing_number_and_progression_result)
+    return start_game(RULE_PROGRESSION,
+                      get_missing_number_and_progression_result)
